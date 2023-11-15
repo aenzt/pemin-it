99c86e51925557a4fed901c23a98202ab3f2417f
17bigb <erwintamba7@gmail.com>
2023-11-15 17:56:15 +0700

    feat(service): adding service and feat get letter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { LegalModule } from './legal/legal.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, LegalModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/legal/legal.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { LegalController } from './legal.controller';
+import { LegalService } from './legal.service';
+
+@Module({
+    controllers: [LegalController],
+    providers: [LegalService]
+  })
+export class LegalModule {}
