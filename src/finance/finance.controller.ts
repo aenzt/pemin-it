120b53c13f9ac4e9bde12915910d44bf8da3956c
Redomeire <redomeire2@gmail.com>
2023-11-23 21:01:14 +0700

    feat(finance): transaction controller and service

--- src/finance/finance.controller.ts
+++ src/finance/finance.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Body,
+  Param,
+  Post,
+  UseGuards,
+  Query,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { FinanceService } from './finance.service';
+import { ResponseMessage } from 'src/transform/response_message.decorator';
+import { AccessTokenGuard } from 'src/auth/common/guards/accessToken.guard';
+import CreateTransactionDTO from './dto/createTransaction.dto';
 
 @Controller('finance')
-export class FinanceController {}
+@ApiTags('Finance')
+export class FinanceController {
+  constructor(private readonly financeService: FinanceService) {}
+
+  @Get('/transactions')
+  @ResponseMessage('Success get all transactions')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  @ApiQuery({
+    name: 'from',
+    type: String,
+    required: false,
+  })
+  @ApiQuery({
+    name: 'to',
+    type: String,
+    required: false,
+  })
+  getAllEmployee(@Query('from') from?: string, @Query('to') to?: string) {
+    return this.financeService.getTransactions(from, to);
+  }
+
+  @Get('/transactions/:id')
+  @ResponseMessage('Success get transaction')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  getEmployeeById(@Param('id') id: string) {
+    return this.financeService.getTransactionById(parseInt(id));
+  }
+
+  @Post('/transactions')
+  @ResponseMessage('Success create new transaction')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  createEmployee(@Body() body: CreateTransactionDTO) {
+    return this.financeService.createTransaction(body);
+  }
+
+  @Delete('/transactions/:id')
+  @ResponseMessage('Success delete transaction')
+  @UseGuards(AccessTokenGuard)
+  @ApiBearerAuth()
+  deleteTransactionById(@Param('id') id: string) {
+    return this.financeService.deleteTransactionById(parseInt(id));
+  }
+}
--- src/finance/finance.service.ts
+++ src/finance/finance.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import CreateTransactionDTO from './dto/createTransaction.dto';
 
 @Injectable()
-export class FinanceService {}
+export class FinanceService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getTransactions(from?: string, to?: string) {
+    const gte = new Date(from);
+    const lte = new Date(to);
+
+    const transactions = await this.prisma.transaction.findMany({
+      where: {
+        createdAt: {
+          gte,
+          lte,
+        },
+      },
+    });
+
+    if (!transactions)
+      throw new NotFoundException('transaction data not found');
+
+    return transactions;
+  }
+
+  async getTransactionById(id: number) {
+    return this.prisma.transaction.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async createTransaction(transaction: CreateTransactionDTO) {
+    return this.prisma.transaction.create({
+      data: {
+        ...transaction,
+      },
+    });
+  }
+
+  async deleteTransactionById(id: number) {
+    return this.prisma.transaction.delete({
+      where: {
+        id,
+      },
+    });
+  }
+}
